8e1b2fbb8db7d4dce821e0f0c430a3900b070041
Chief <kameyaw14@gmail.com>
2025-10-13 22:02:56 +0000

    fix: Correct BASE_URL assignment to include API path

--- src/contexts/AppContext.jsx
+++ src/contexts/AppContext.jsx
@@ -8,7 +8,7 @@ const AppContext = createContext();
 axios.defaults.withCredentials = true;
 
 export const AppProvider = ({ children }) => {
-  const BASE_URL = import.meta.env.VITE_SERVER_URL || 
+  const BASE_URL = `${import.meta.env.VITE_SERVER_URL}/api/` || 
     (import.meta.env.VITE_ENV === "development"
       ? "http://localhost:3100/api/"
       : `${import.meta.env.VITE_SERVER_URL}/api/`);
